da986aa245c298958cf462013e588e144ee62bb8
Alex Wang <alexwang3150@gmail.com>
2019-03-26 22:26:57 -0400

    first components and styled components

---
+++ quiz/src/Question.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const qdiv =  styled.div`
+
+`
+
+class Question extends Component {
+  render() {
+    return (
+      <div>
+          Is water wet?
+      </div>
+    );
+  }
+}
+
+export default Question;
