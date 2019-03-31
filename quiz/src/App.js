4f2737b3deb67c247c149f198cd6494762f262a0
Alex Wang <alexwang3150@gmail.com>
2019-03-31 16:28:30 -0400

    finished header

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import QuestionContainer from './QuestionContainer';
-import Answers from './Answers.js'
+import Answers from './Answers.js';
+import Header from './Header.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
-<<<<<<< HEAD
-        <Question>
-          
-        </Question>
-        
-=======
-        <QuestionContainer/>
-        <Answers/>
->>>>>>> 0cf809cfecd29271bb3ba9a5567a096c297a4479
-      </div>
+      <Header/>
     );
   }
 }
