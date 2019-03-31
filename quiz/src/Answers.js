2deb382c9f2cccdae096e023dd2e3c0953aefe44
Kenevian <Katon.vanetten@gmail.com>
2019-03-31 15:35:29 -0400

    moved Files

--- quiz/src/Answers.js
+++ quiz/src/Answers.js
@@ -5,16 +5,29 @@ let AnswerComp = styled.div`
 background-color: red;
 width: 300px;
 height: 300px;
+position:relative;
+text-align: center;
+
 `
-let thumbnail = styled.img`
-src: "./testImage.jpeg";
+let AnswerText = styled.p`
+font-size: 20px;
+color: white;
+`
+let Thumbnail = styled.img`
+margin: 10px;
+position: relative;
+transition: transform .5s;
+&:hover{
+  transform: scale(1.1);
+  background-color: white;
+}
 `
 
 class Answer extends Component {
   render() {
     return (
       <AnswerComp>
-      <thumbnail/>WHOA
+      <AnswerText>Netflix And Chill</AnswerText><Thumbnail src = "./testImage.jpeg"></Thumbnail>
       </AnswerComp>
     );
   }
