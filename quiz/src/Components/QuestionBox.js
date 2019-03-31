9647fb2b718295918ef38f88b1ef4221f2cc168b
RaindragonD <xlfdlyl@sina.com>
2019-03-31 16:04:00 -0400

    question box

--- quiz/src/Components/QuestionBox.js
+++ quiz/src/Components/QuestionBox.js
@@ -4,18 +4,20 @@ import styled from 'styled-components'
 const QuestionBox = styled.div`
   background: #62A8E5;
   color: white;
-  width: 100%;
+  width: 90%;
   padding-top: 10px;
   padding-bottom: 10px;
   padding-left: 10px;
   padding-right: 10px;
+  text-align: center; 
+  margin: 10px;
 `
 
 const questionBox = (props) =>{
     return (
-        <QuestionBox>
-          {props.question}
-        </QuestionBox>
+      <QuestionBox>
+        {props.question}
+      </QuestionBox>
     )
 }
 
--- quiz/src/stories/index.js
+++ quiz/src/stories/index.js
@@ -21,7 +21,8 @@ storiesOf('Button', module)
     </Button>
   ));
 
-storiesOf('QuestionBox', module).add('QuestionBox', () => <QuestionBox question="Some Question"/>);
 
-const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-storiesOf('TitleBox', module).add('TitleBox', () => <TitleBox title = "some title" description = {description}/>);
+
+const ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+storiesOf('TitleBox', module).add('TitleBox', () => <TitleBox title = "some title" description = {ipsum}/>);
+storiesOf('QuestionBox', module).add('QuestionBox', () => <QuestionBox question={ipsum}/>);
