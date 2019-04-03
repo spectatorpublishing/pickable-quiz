ff413d3307847ab18866d43214517da91b792a63
Rounak Bera <rounakbera@gmail.com>
2019-04-02 21:34:03 -0400

    merge 2

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-// import QuestionContainer from './QuestionContainer';
-// import Answers from './Answers.js'
+import QuestionContainer from './QuestionContainer';
+import Answers from './Answers.js'
 import Results from './Results.js'
-import Answers from './Answers.js';
 import Header from './Header.js';
 
 class App extends Component {
@@ -11,12 +9,14 @@ class App extends Component {
   render() {
     
     return (
+      <div>
       <Header/>
       <div>
         <QuestionContainer/>
         <Answers answerImage = { "./testImage.jpeg" } text = { "Netflix And Chill" }></Answers>
       </div>
       <Results title = "Lorem ipsum dolor" imgAlt = "" description = "Lorem ipsum dolor sit amet, eam no doming oportere. Et virtute explicari mei, vix no salutandi moderatius complectitur, cu omnium."/>
+      </div>
     );
   }
 }
