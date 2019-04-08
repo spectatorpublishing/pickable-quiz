475ebcd795d12d3625540c58bfdaab54d467f047
Rounak Bera <rounakbera@gmail.com>
2019-04-08 00:45:43 -0400

    quick header change

--- quiz/src/Header.js
+++ quiz/src/Header.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components'
 import img from './testbackground.jpg'
 
 const Wrapper = styled.header`
-    min-height: 50vh;
+    min-height: 30vh;
     display: flex;
     align-content: flex-start;
     justify-content: center;
